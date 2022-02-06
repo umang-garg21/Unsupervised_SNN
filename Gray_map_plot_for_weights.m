b5f7880de2df94f9ab375b983de689145d12ba68
Umang Garg <93625117+UMANG-GARG-UCSB@users.noreply.github.com>
2022-02-05 23:20:38 -0800

    Update Gray_map_plot_for_weights.m

--- Gray_map_plot_for_weights.m
+++ Gray_map_plot_for_weights.m
@@ -1,12 +1,23 @@
 
+initial_weights_n1 = [ 1 0 0 1 2; 0 3 2 3 0; 2 2 1 3 3; 1 3 0 0 2; 3 1 1 0 1 ]
+initial_weights_n2 = [ 0 2 3 0 1; 2 0 2 0 0; 1 2 3 1 2; 0 1 1 0 1; 2 3 1 3 0 ]
 
-A = [0 0 0 0 1; 0 3 3 3 0; 1 3 0 3 2; 0 3 3 3 1; 2 0 0 0 0 ] 
-B = [0 3 3 0 0; 1 0 3 0 0; 0 1 3 0 1; 0 0 3 0 0; 1 3 3 3 0 ]
+
+learnt_n1 = [0 0 0 0 1; 0 3 3 3 0; 1 3 0 3 2; 0 3 3 3 1; 2 0 0 0 0 ] 
+learnt_n2 = [0 3 3 0 0; 1 0 3 0 0; 0 1 3 0 1; 0 0 3 0 0; 1 3 3 3 0 ]
 
 figure(1);
-imagesc(A)
+imagesc(learnt_n1)
 colormap(flipud(gray))
 
 figure(2);
-imagesc(B)
+imagesc(learnt_n2)
+colormap(flipud(gray))
+
+figure(3);
+imagesc(initial_weights_n1)
+colormap(flipud(gray))
+
+figure(4);
+imagesc(initial_weights_n2)
 colormap(flipud(gray))
